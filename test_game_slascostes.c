1a53c325987bf5339ab654e6b9f1582db0a916be
slascostes <sylvain.lascostes@u-bordeaux.fr>
2019-10-21 16:57:59 +0200

    ajout.c

--- test_game_slascostes.c
+++ test_game_slascostes.c
@@ -3,20 +3,60 @@
 #include "game.h"
 #include "game_io.h"
 
-bool test_game_new(){
-    game g= game_new(color *cells,nb_moves_max);
+
+bool test_game_new(color *cells, unsigned int nb_moves_max){
+    game g= game_new(cells,nb_moves_max);
     if (g == NULL){
         return false;
     }
-    if (nb_max_moves <= 0){
+    if (nb_moves_max <= 0){
         return false;
     }
-    for(unsigned int x=0 ; x<12 ; x++){
-        for(unsigned int y=0 ; y<12; y++){
-            if (game_cell_current_color(g, x, y) != cells){
+    for(unsigned int x=0 ; x<nb_moves_max ; x++){
+        for(unsigned int y=0 ; y<nb_moves_max ; y++){
+            if (game_cell_current_color(g, x, y) != *cells){
                 return false;
             }
         }
     }
     return true;
 }
+
+bool test_game_nb_moves_max(){
+    game g = game_new_empty();
+    if (g == NULL){
+        return false;
+    }
+    unsigned int nb_max = game_nb_moves_max(g);
+    if (nb_max < 0){
+        return false;
+    }
+    return true;
+}
+
+bool test_game_copy(cgame g){
+    if (g == NULL){
+        return false;
+    }
+    cgame g2 = game_copy(g);
+    if (g2 == NULL){
+        return false;
+    }
+    unsigned int nb_max = game_nb_moves_max(g);
+    if (test_game_nb_moves_max() == true){
+        for(unsigned int x=0 ; x<nb_max ; x++){
+            for(unsigned int y=0 ; y<nb_max ; y++){
+                if (game_cell_current_color(g2, x, y) != game_cell_current_color(g, x, y) ){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main (){
+    return EXIT_SUCCESS;
+ }
