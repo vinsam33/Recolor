b9cd45d7fab64e402b0a7533d25b1f25b70d9126
slascostes <sylvain.lascostes@u-bordeaux.fr>
2019-10-25 11:46:17 +0200

    test_game_copy a jour et modif Makefile

--- test_game_slascostes.c
+++ test_game_slascostes.c
@@ -1,5 +1,8 @@
-#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "game.h"
 #include "game_io.h"
 
@@ -38,36 +41,85 @@ bool test_game_nb_moves_max(){
     return true;
 }
 
-bool test_game_copy(game g){
+bool test_game_copy(){
+    color cells [144] = {0,0,0,2,0,2,1,0,1,0,3,0,0,3,3,1,1,1,1,3,2,0,1,0,1,0,1,2,3,2,3,2,0,3,3,2,2,3,1,0,3,2,1,1,1,2,2,0,2,1,2,3,3,3,3,2,0,1,0,0,0,3,3,0,1,1,2,3,3,2,1,3,1,1,2,2,2,0,0,1,3,1,1,2,1,3,1,3,1,0,1,0,1,3,3,3,0,3,0,1,0,0,2,1,1,1,3,0,1,3,1,0,0,0,3,2,3,1,0,0,1,3,3,1,1,2,2,3,2,0,0,2,2,0,2,3,0,1,1,1,2,3,0,1};
+    game g = game_new(cells, 11);
     if(g == NULL){
         return false;
     }
     game g2 = game_copy(g);
     if(g2 == NULL){
+        game_delete(g);
+        game_delete(g2);
         return false;
     }
     if(game_is_over(g) != game_is_over(g2)){
+        game_delete(g);
+        game_delete(g2);
         return false;
     }
     if (game_nb_moves_max(g) != game_nb_moves_max(g2)){
+        game_delete(g);
+        game_delete(g2);
         return false;
     }
     if(game_nb_moves_cur(g) != game_nb_moves_cur(g2)){
+        game_delete(g);
+        game_delete(g2);
         return false;
     }
     //unsigned int nb_max =  game_nb_moves_max(g);
     for(unsigned int x=0 ; x<SIZE ; x++){
         for(unsigned int y=0 ; y<SIZE ; y++){
             if (game_cell_current_color(g2, y, x) != game_cell_current_color(g, y, x)){
+                game_delete(g);
+                game_delete(g2);
                 return false;
             }
         }
     }
+    game_delete(g);
+    game_delete(g2);
     return true;
 }
 
 
-int main (){
+void usage(int argc, char *argv[]) {
+  fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
+  exit(EXIT_FAILURE);
+}
+
+/* ********** MAIN ROUTINE ********** */
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) usage(argc, argv);
+  color cells [144] = {0,0,0,2,0,2,1,0,1,0,3,0,0,3,3,1,1,1,1,3,2,0,1,0,1,0,1,2,3,2,3,2,0,3,3,2,2,3,1,0,3,2,1,1,1,2,2,0,2,1,2,3,3,3,3,2,0,1,0,0,0,3,3,0,1,1,2,3,3,2,1,3,1,1,2,2,2,0,0,1,3,1,1,2,1,3,1,3,1,0,1,0,1,3,3,3,0,3,0,1,0,0,2,1,1,1,3,0,1,3,1,0,0,0,3,2,3,1,0,0,1,3,3,1,1,2,2,3,2,0,0,2,2,0,2,3,0,1,1,1,2,3,0,1};
+  /* game g = game_new(cells, 11);*/
+  // start test
+  fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
+  bool ok = false;
+  if (strcmp("game_new", argv[1]) == 0)
+    ok = test_game_new(cells, 11);
+  else if (strcmp("game_nb_moves_max", argv[1]) == 0)
+    ok = test_game_nb_moves_max();
+  else if (strcmp("game_copy", argv[1]) == 0)
+    ok = test_game_copy();
+  else {
+    fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  // print test result
+  if (ok) {
+    fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
+    return EXIT_SUCCESS;
+  } else {
+    fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+}
+
+/*int main (){
     color cells [144] = {0,0,0,2,0,2,1,0,1,0,3,0,0,3,3,1,1,1,1,3,2,0,1,0,1,0,1,2,3,2,3,2,0,3,3,2,2,3,1,0,3,2,1,1,1,2,2,0,2,1,2,3,3,3,3,2,0,1,0,0,0,3,3,0,1,1,2,3,3,2,1,3,1,1,2,2,2,0,0,1,3,1,1,2,1,3,1,3,1,0,1,0,1,3,3,3,0,3,0,1,0,0,2,1,1,1,3,0,1,3,1,0,0,0,3,2,3,1,0,0,1,3,3,1,1,2,2,3,2,0,0,2,2,0,2,3,0,1,1,1,2,3,0,1};
     game g = game_new(cells, 11);
     if(test_game_new(cells,11)){
@@ -89,4 +141,4 @@ int main (){
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
- }
+ }*/
