66a009c8abdb06a49fe2ea22e98ac78ca176aba1
vsamson <vincent.samson@etu.u-bordeaux.fr>
2019-10-25 19:23:44 +0200

    test testvsamson.cv3

--- test_game_vsamson.c
+++ test_game_vsamson.c
@@ -8,16 +8,25 @@ typedef unsigned int uint;
 
 bool test_game_new_empty(){
     color cells [144] = {0,0,0,2,0,2,1,0,1,0,3,0,0,3,3,1,1,1,1,3,2,0,1,0,1,0,1,2,3,2,3,2,0,3,3,2,2,3,1,0,3,2,1,1,1,2,2,0,2,1,2,3,3,3,3,2,0,1,0,0,0,3,3,0,1,1,2,3,3,2,1,3,1,1,2,2,2,0,0,1,3,1,1,2,1,3,1,3,1,0,1,0,1,3,3,3,0,3,0,1,0,0,2,1,1,1,3,0,1,3,1,0,0,0,3,2,3,1,0,0,1,3,3,1,1,2,2,3,2,0,0,2,2,0,2,3,0,1,1,1,2,3,0,1};
-    game g = game_new(cells, 10);
+    game g = game_new(cells, 11);
     if(g==NULL){
         return false;
+    }
+    for (unsigned int y=0;y<SIZE;y++){
+        for (unsigned int x=0 ;x<SIZE;x++){
+            if (game_cell_current_color(g,y,x) != RED){
+                return false;
+            }if (game_nb_moves_max(g)!=0){
+                return false;
+            }
+        }
     }
     game_new_empty();
     return true;
 }
 bool test_game_cell_current_color(){
     game g = game_new_empty();
-    if(!g){
+    if(g==NULL){
         return false;
     }for (uint y=0; y < SIZE; y++){
         for (uint x=0; x < SIZE ; x++){
@@ -53,14 +62,14 @@ int main(void){
     }
     bool color = test_game_cell_current_color(g,2,SIZE-1);
     if (color ==true){
-        fprintf(stderr,"EXECUTING OF game_cell_current_color IS : REUSSITE\n\n");
+        fprintf(stderr,"EXECUTING OF game_cell_current_color IS : REUSITE\n\n");
     }else{
         fprintf(stderr,"EXECUTING OF game_cell_current_color IS : FAILURE\n\n ");
         return EXIT_FAILURE;
     }
     bool supr = test_game_delete();
     if(supr==true){
-        fprintf(stderr,"EXECUTING OF game_delete IS : REUSSITE\n\n ");
+        fprintf(stderr,"EXECUTING OF game_delete IS : REUSITE\n\n ");
 
     }else{
         fprintf(stderr,"EXECUTING OF game_delete IS : FAILURE\n\n ");
