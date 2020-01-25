3f7fc14ce0be34794215660f53f0e1696d33557a
elhdiallo <elhadj-mamadou.diallo@etu.u-bordeaux.fr>
2020-01-25 13:23:51 +0100

    modif game_save

--- game_io.c
+++ game_io.c
@@ -6,6 +6,22 @@
 
 game game_load(char *filename) { return NULL; }
 
-void game_save(cgame g, char *filename) {}
-
+void game_save(cgame g, char *filename) {
+    if ( filename==NULL ){
+        fprintf(stderr, "NULL pointer");
+        exit(EXIT_FAILURE);
+    }
+    FILE * f=fopen(filename, "r");
+    if(f==NULL){
+        fprintf(stderr, "NULL pointer");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(f, "%d %d %d %d \n", game_width(g), game_height(g), g->nb_max, game_is_wrapping(g));
+    for(uint x=0; x < game_width(g); x++){
+        for(uint y=0; game_height(g); y++){
+            fprintf(f, "%d ", tab[x][y]);
+        }
+    }
+    fclose(f);
+}
 int main(void) { return EXIT_SUCCESS; }
