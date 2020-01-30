f55a3d132171fd96cd811c681884b2b9dc188a69
vsamson <vincent.samson@etu.u-bordeaux.fr>
2020-01-30 20:48:07 +0100

    modif game_io erreurs dans save

--- game_io.c
+++ game_io.c
@@ -51,12 +51,11 @@ void game_save(cgame g, char *filename) {
         fprintf(stderr, "NULL pointer");
         exit(EXIT_FAILURE);
     } 
-    fprintf(f, "%d %d %d %d \n", game_width(g), game_height(g), g->nb_max, game_is_wrapping(g));
+    fprintf(f, "%d %d %d %d \n", game_width(g), game_height(g), game_nb_moves_max(g), game_is_wrapping(g));
     for(uint x=0; x < game_width(g); x++){
         for(uint y=0; game_height(g); y++){
-            fprintf(f, "%d ", g->tab[x][y]);
+            fprintf(f, "%d ", game_cell_current_color(g,x,y));
         }
     }
     fclose(f);
 }
-int main(void) { return EXIT_SUCCESS; }
