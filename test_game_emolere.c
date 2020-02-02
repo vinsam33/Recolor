3093b069b293cf51eac5ea5cb1e0c81a05a1c6e1
emolere <emeric.molere@etu.u-bordeaux.fr>
2020-02-02 13:36:02 +0100

    Implémentation test_game_load

--- test_game_emolere.c
+++ test_game_emolere.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include "game_io.h"
+#include "game_io.h"
+
+#define MAXLINELEN 4096
 
 bool test_game_set_max_moves(uint max) {
   game g = game_new_empty_ext(12, 12, false); 
@@ -125,6 +128,138 @@ bool test_game_new_empty_ext(uint width, uint height, bool wrapping){
   return true;
 }
 
+bool test_game_load(){
+  game g = game_load("default_game.rec.txt"); // Wrapping en true
+  if ("default_game.rec.txt"==NULL){
+    fprintf(stderr, "Null pointer\n");
+    exit(EXIT_FAILURE);
+  }
+  FILE * f = fopen("default_game.rec.txt","r");
+  if (f == NULL){
+    game_delete(g);
+    exit(EXIT_FAILURE);
+  }
+  char *s = malloc(sizeof (char)*MAXLINELEN);
+  if(s==NULL){
+    fprintf(stderr, "Null pointer\n");
+    game_delete(g);
+    fclose(f);
+    exit(EXIT_FAILURE);
+  }
+  s = fgets(s, MAXLINELEN, f);
+  char *d = strtok(s, " \n");
+  uint w = atoi(d);
+
+  d = strtok(NULL, " \n");
+  uint h = atoi(d); 
+
+  d = strtok(NULL, " \n");
+  uint nb_max = atoi(d); 
+
+  d = strtok(NULL, " \n");
+  bool wrapping = true;
+  if (d=='N') wrapping = false;
+
+  if(game_is_wrapping(g) != wrapping || game_height(g) != h || game_width(g) != w || game_nb_moves_max(g) != nb_max){
+    free(s);
+    game_delete(g);
+    fclose(f);
+    return false;
+  }
+
+  color *cells = malloc(sizeof(color)*MAXLINELEN);
+  int i = 0;
+  while (fgets(s, MAXLINELEN, f) != NULL){
+    char *tok = strtok(s, " \n");
+    while (tok != NULL){
+      int n = atoi(tok);
+      cells[i] = n;
+      i++;
+      tok = strtok(NULL, " \n");
+    }
+  }
+  for (uint x=0; x < w; x++){
+    for (uint y=0; y < h; y++){
+      if (game_cell_current_color(g, x, y) != cells[y * w + x]){
+        free(s);
+        free(cells);
+        game_delete(g);
+        fclose(f);
+        return false;
+      }
+    }
+  }
+  free(s);
+  free(cells);
+  game_delete(g);
+  fclose(f);
+  game g2 = game_load("horizontal_game2N.rec.txt"); // Wrapping en false
+  if ("horizontal_game2N.rec.txt"==NULL){
+    fprintf(stderr, "Null pointer\n");
+    exit(EXIT_FAILURE);
+  }
+  FILE * f2 = fopen("horizontal_game2N.rec.txt","r");
+  if (f2 == NULL){
+    game_delete(g2);
+    exit(EXIT_FAILURE);
+  }
+  char *s2 = malloc(sizeof (char)*MAXLINELEN);
+  if(s2==NULL){
+    fprintf(stderr, "Null pointer\n");
+    game_delete(g2);
+    fclose(f2);
+    exit(EXIT_FAILURE);
+  }
+  s2 = fgets(s2, MAXLINELEN, f2);
+  char *d2 = strtok(s2, " \n");
+  uint w2 = atoi(d2);
+
+  d2 = strtok(NULL, " \n");
+  uint h2 = atoi(d2);
+
+  d2 = strtok(NULL, " \n");
+  uint nb_max2 = atoi(d2);
+
+  d2 = strtok(NULL, " \n");
+  bool wrapping2 = true;
+  if (d2=='N') wrapping2 = false;
+
+  if(game_is_wrapping(g2) != wrapping2 || game_height(g2) != h2 || game_width(g2) != w2 || game_nb_moves_max(g2) != nb_max2){
+    free(s2);
+    game_delete(g2);
+    fclose(f2);
+    return false;
+  }
+
+  color *cells2 = malloc(sizeof(color)*MAXLINELEN);
+  uint i2 = 0;
+  while (fgets(s2, MAXLINELEN, f2) != NULL){
+    char *tok = strtok(s2, " \n");
+    while (tok != NULL){
+      int n = atoi(tok);
+      cells2[i2] = n;
+      i2++;
+      tok = strtok(NULL, " \n");
+    }
+  }
+  for (uint x=0; x < w2; x++){
+    for (uint y=0; y < h2; y++){
+      if (game_cell_current_color(g2, x, y) != cells2[y * w2 + x]){
+        free(s2);
+        free(cells2);
+        game_delete(g2);
+        fclose(f2);
+        return false;
+      }
+    }
+  }
+  free(s2);
+  free(cells2);
+  game_delete(g2);
+  fclose(f2);
+  return true;
+}
+
 /*** ***** MAIN ***** ***/
 
 int main(void) {
@@ -143,7 +278,7 @@ int main(void) {
   if (Agree) {
     fprintf(stderr, "Execution of game_play_one_move : Success\n\n");
   } else {
-    fprintf(stderr, "Execution of game_play_one_move : Denied\n\n");
+    fprintf(stderr, "Execution of game_play_one_move : Failuren\n");
     return EXIT_FAILURE;
   }
   
@@ -152,7 +287,7 @@ int main(void) {
   if (Agree) {
     fprintf(stderr, "Execution of game_restart : Success\n\n");
   } else {
-    fprintf(stderr, "Execution of game_restart : Denied\n\n");
+    fprintf(stderr, "Execution of game_restart : Failure\n\n");
     return EXIT_FAILURE;
   }
   
@@ -161,9 +296,17 @@ int main(void) {
   if (Agree) {
     fprintf(stderr, "Execution of game_new_empty_ext : Success\n\n");
   } else {
-    fprintf(stderr, "Execution of game_new_empty_ext : Denied\n\n");
+    fprintf(stderr, "Execution of game_new_empty_ext : Failure\n\n");
     return EXIT_FAILURE;
   }
 
+  printf("-- Start test of game_load --\n"); 
+  Agree = test_game_load(); 
+  if (Agree) {
+    fprintf(stderr, "Execution of game_load : Success\n\n");
+  } else{
+    fprintf(stderr, "Execution of game_load : Failure\n\n");
+    return EXIT_FAILURE; 
+  }
   return EXIT_SUCCESS;
 }
