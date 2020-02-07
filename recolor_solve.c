792672260bdee7a56fdf4a898d9a8d4f33ca8d73
Elhadj Mamadou Diallo <elhadj-mamadou.diallo@etu.u-bordeaux.fr>
2020-02-07 11:27:29 +0100

    create file

---
+++ recolor_solve.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "game.h"
+#include "game_io.h"
+#include "string.h"
+#include "time.h"
+
+
+void FIND_ONE(){
+
+    return NULL;
+}
+void NB_SOL(){
+
+    return NULL;
+}
+void NB_SOL(){
+
+    return NULL;
+}
